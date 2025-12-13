5b4fdcedf74a2deb0ba9bcfdb4d50ba648ef07ba
lubava <zero3188@yandex.ru>
2025-12-12 19:18:05 -0800

    поправила локаторы

--- locators/create_add_locators.py
+++ locators/create_add_locators.py
@@ -6,15 +6,15 @@ class CreateAdLocators:
     TITLE_INPUT = (By.CSS_SELECTOR, "input[placeholder='Название']")
     DESCRIPTION_INPUT = (By.CSS_SELECTOR, "textarea[placeholder='Описание товара']")
     PRICE_INPUT = (By.CSS_SELECTOR, "input[placeholder='Стоимость']")
-    CATEGORY_DROPDOWN = (By.XPATH, '//*[@id="root"]/div/div[2]/div/form/div[2]/div[2]/div[1]/button')
+    CATEGORY_DROPDOWN = (By.XPATH, "//input[@name='category']/following-sibling::button[contains(@class, 'dropDownMenu_arrowDown__pfGL1')]")
     CATEGORY = (By.XPATH, "//span[contains(text(), 'Книги')]")
-    CITY_DROPDOWN = (By.XPATH, '//*[@id="root"]/div/div[2]/div/form/div[3]/div[1]/button')
+    CITY_DROPDOWN = (By.XPATH, "//input[@value='Москва' and @name='city']/following-sibling::button")
     CITY = (By.XPATH, "//span[contains(text(), 'Санкт-Петербург')]")
     PUBLISH_BUTTON = (By.XPATH, "//button[contains(text(), 'Опубликовать')]")
 
     # Radio buttons
-    CONDITION_NEW = (By.XPATH, '//*[@id="root"]/div/div[2]/div/form/fieldset/div/div[1]/div')
-    CONDITION_USED = (By.XPATH, '//*[@id="root"]/div/div[2]/div/form/fieldset/div/div[2]/div')
+    CONDITION_NEW = (By.XPATH, "//div[.//input[@value='Новый'] and contains(@class, 'radioUnput_shell__Wtdwe')]")
+    CONDITION_USED = (By.XPATH, "//div[.//input[@value='Б/У'] and contains(@class, 'radioUnput_shell__Wtdwe')]")
     
     # Модальное окно для неавторизованных
     AUTH_MODAL_TITLE = (By.XPATH, "//h1[contains(text(), 'Чтобы разместить объявление, авторизуйтесь')]")
--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 class MainPageLocators:
     # Кнопки
     LOGIN_REGISTER_BUTTON = (By.XPATH, "//button[contains(text(), 'Вход и регистрация')]")
-    CREATE_AD_BUTTON = (By.XPATH, "//button[contains(text(), 'Разместить объявление')]")
+    CREATE_AD_BUTTON = (By.CSS_SELECTOR, "button.buttonPrimary.inButtonText")
     LOGOUT_BUTTON = (By.XPATH, "//button[contains(text(), 'Выйти')]")
 
     # Элементы пользователя
@@ -14,4 +14,5 @@ class MainPageLocators:
         "h3.profiletext.name, h3[class*='profiletext'], h3[class*='name']")
     
     # Навигация
-    PROFILE_LINK = (By.XPATH, '//*[@id="root"]/div/div[1]/div/div[1]/button')
+    PROFILE_LINK = (By.CSS_SELECTOR,
+        "button.circleSmall, .circleSmall svg, [class*='circleSmall']")
